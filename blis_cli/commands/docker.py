6f4f5b59762035e07cfe31f5abda40e719ed6ac3
Mitchell Rysavy <mitchell.rysavy@gmail.com>
2023-09-28 01:07:55 +0000

    blis docker install

--- blis_cli/commands/docker.py
+++ blis_cli/commands/docker.py
@@ -1,6 +1,13 @@
 import click
-import docker
+import docker as lib_docker
+import os
+import requests
+import shutil
 
+from blis_cli.util import bash
+from blis_cli.util import config
+from blis_cli.util import docker_util as docker
+from blis_cli.util import emoji
 from blis_cli.util import environment as env
 from blis_cli.util import packages
 
@@ -11,11 +18,76 @@ def entrypoint():
 
 
 @click.command
+def install():
+    # If Docker is installed, and we are in the docker group, we will not need root privileges.
+    if not (env.in_docker_grp() and docker.installed()):
+        if not (os.geteuid() == 0 or env.can_sudo()):
+            click.secho(
+                "Docker must be installed. You must run this script as root or have passwordless sudo privileges.",
+                fg="red",
+            )
+            exit(1)
+
+        install_docker()
+
+        # Returns a success code because we probably succeeded and installed Docker, but we need to log out and log back in.
+        exit(0)
+
+    # We might have Docker installed, but not be in the docker group.
+    if docker.installed() and not env.in_docker_grp():
+        click.secho(
+            "You have Docker installed, but you are not in the docker group.",
+            fg="yellow",
+        )
+        # Try to fix the problem...
+        if env.can_sudo():
+            bash.sudo("usermod -aG docker $USER")
+            click.echo("Please log out and log back in, and run this command again.")
+            exit(0)
+        else:
+            click.secho(
+                "You must run this script as root or have passwordless sudo privileges.",
+                fg="red",
+            )
+        exit(1)
+        
+    click.secho("Docker is installed!", fg="green")
+
+
+def install_docker():
+    click.echo("Setting up Docker... ")
+    packages.remove(["docker", "docker-engine", "docker.io", "containerd", "runc"])
+    packages.install(["ca-certificates", "curl", "gnupg", "lsb-release"])
+    bash.sudo("mkdir -p /etc/apt/keyrings")
+    bash.run(
+        "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o - | sudo tee /etc/apt/keyrings/docker.gpg >/dev/null"
+    )
+    bash.run(
+        'echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null'
+    )
+    packages.apt_update()
+    packages.install(
+        [
+            "docker-ce",
+            "docker-ce-cli",
+            "containerd.io",
+            "docker-compose-plugin",
+            "docker-buildx-plugin",
+        ]
+    )
+    bash.sudo("usermod -aG docker $USER")
+    bash.sudo("systemctl enable docker.service")
+    bash.sudo("systemctl start docker.service")
+    click.secho("Success!", fg="green")
+    click.echo("Please log out and log back in, and run this command again.")
+
+
+@click.command
 def purge():
     if not env.can_sudo():
         click.secho("You must have root privileges to run this.", fg="red")
         exit(1)
-    
+
     packages.remove(["docker", "docker-engine", "docker.io", "containerd", "runc"])
     packages.remove(
         [
@@ -31,12 +103,28 @@ def purge():
 
 @click.command
 def status():
-    client = docker.from_env()
-    click.echo("containers:")
+    click.echo(
+        f"Passwordless sudo: {emoji.GREEN_CHECK if env.can_sudo() else emoji.RED_X}"
+    )
+    click.echo(
+        f"Docker is installed: {emoji.GREEN_CHECK if docker.installed() else emoji.RED_X}"
+    )
+    click.echo(
+        f"Docker Compose: {emoji.GREEN_CHECK if docker.compose() is not None else emoji.RED_X}"
+    )
+    click.echo(
+        f"User '{env.user()}' in 'docker' group: {emoji.GREEN_CHECK if env.in_docker_grp() else emoji.RED_X}"
+    )
+
+    click.echo()
+
+    client = lib_docker.from_env()
+    click.echo("Containers:")
     for c in client.containers.list():
         click.echo(c.name)
         click.echo(c.image)
 
 
+entrypoint.add_command(install)
 entrypoint.add_command(purge)
 entrypoint.add_command(status)
--- blis_cli/commands/install.py
+++ blis_cli/commands/install.py
@@ -1,7 +1,6 @@
 import click
 import os
 import docker as lib_docker
-import requests
 import shutil
 
 from blis_cli.util import bash
@@ -13,46 +12,27 @@ from blis_cli.util import packages
 
 
 def install():
-    # If Docker is installed, and we are in the docker group, we will not need root privileges.
-    if not (env.in_docker_grp() and docker.installed()):
-        if not (os.geteuid() == 0 or env.can_sudo()):
-            click.secho(
-                "Docker must be installed. You must run this script as root or have passwordless sudo privileges.",
-                fg="red",
-            )
-            return 1
+    if not docker.installed():
+        click.secho("You must install Docker before continuing. Please run:", fg="red")
+        click.echo("  sudo blis docker install")
+        return 1
 
-        install_docker()
-
-        # Returns a success code because we probably succeeded and installed Docker, but we need to log out and log back in.
-        return 0
-
-    # We might have Docker installed, but not be in the docker group.
-    if docker.installed() and not env.in_docker_grp():
-        click.secho(
-            "You have Docker installed, but you are not in the docker group.",
-            fg="yellow",
-        )
-        # Try to fix the problem...
-        if env.can_sudo():
-            bash.sudo("usermod -aG docker $USER")
-            click.echo("Please log out and log back in, and run this command again.")
-            return 0
-        else:
-            click.secho(
-                "You must run this script as root or have passwordless sudo privileges.",
-                fg="red",
-            )
+    if not env.in_docker_grp():
+        click.secho("You must be in the Docker group to continue. Please run", fg="red")
+        click.echo("  sudo blis docker install")
         return 1
 
     # We have Docker installed and we are in the docker group.
     version = lib_docker.from_env().version()
     click.echo(f"Docker version: {click.style(version['Version'], fg='green')}")
 
+    if not click.confirm(
+        "BLIS has already been installed in ~/.blis. Do you want to overwrite the configuration?"
+    ):
+        return 0
+
     config.make_basedir()
     copy_docker_files()
-    # if not os.path.exists(config.compose_file()):
-    #     download_docker_files()
 
     if config.validate_compose():
         click.echo("docker-compose.yml is valid.")
@@ -63,47 +43,6 @@ def install():
     run_blis_and_setup_db()
 
     click.secho("You are ready to rock!", fg="green")
-
-
-def install_docker():
-    click.echo("Setting up Docker... ")
-    packages.remove(["docker", "docker-engine", "docker.io", "containerd", "runc"])
-    packages.install(["ca-certificates", "curl", "gnupg", "lsb-release"])
-    bash.sudo("mkdir -p /etc/apt/keyrings")
-    bash.run(
-        "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o - | sudo tee /etc/apt/keyrings/docker.gpg >/dev/null"
-    )
-    bash.run(
-        'echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null'
-    )
-    packages.apt_update()
-    packages.install(
-        [
-            "docker-ce",
-            "docker-ce-cli",
-            "containerd.io",
-            "docker-compose-plugin",
-            "docker-buildx-plugin",
-        ]
-    )
-    bash.sudo("usermod -aG docker $USER")
-    bash.sudo("systemctl enable docker.service")
-    bash.sudo("systemctl start docker.service")
-    click.secho("Success!", fg="green")
-    click.echo("Please log out and log back in, and run this command again.")
-
-
-def download_docker_files():
-    click.echo("Downloading docker-compose.yml... ", nl=False)
-    r = requests.get(
-        "https://raw.githubusercontent.com/C4G/BLIS/master/docker/docker-compose.yml"
-    )
-    if r.status_code != 200:
-        click.secho("Failed to download docker-compose.yml", fg="red")
-        return False
-    with open(config.compose_file(), "wb") as f:
-        f.write(r.content)
-    click.secho("Success!", fg="green")
 
 
 def copy_docker_files():
